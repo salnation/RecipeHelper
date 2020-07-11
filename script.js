2abb0531f9ef1bc20d1c6afacf0966f0dd801bfe
CorreaPH <correaph@gmail.com>
2020-07-11 17:26:25 -0300

    Merging Food Search API and Recipe Search API. Adding github link and edaman badge.

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+/*Triggered when 'Search Button' is clicked, this event will then decides which function (API) will be called, based on the selected radio button*/
 $("#searchBtn").click(function () {
-    /*Recipe Search API*/
+    if (!$("#searchInput").val()) {
+        alert("Please, type something to be searched!");
+    } else if ($("#recipeSearchRadio").prop("checked")) {
+        recipeSearch();
+    } else {
+        foodSearch();
+    }
+});
+/*This function uses the Recipe Search API from edaman and fill container div with 5 recipes based on what the user have type in the search input*/
+function recipeSearch() {
     var appId_RSA = "3adbf29f";
     var appKey_RSA = "edb57596dcb58fff67450bf3379f2078";
-    var foodDesc = $("#recipeSearch").val();
-    var queryUrl = "https://api.edamam.com/search?q=" + foodDesc + "&app_id=" + appId_RSA + "&app_key=" + appKey_RSA + "&from=0&to=5";
+    var searchInput = $("#searchInput").val();
+    var queryUrl = "https://api.edamam.com/search?q=" + searchInput + "&app_id=" + appId_RSA + "&app_key=" + appKey_RSA + "&from=0&to=5";
     var settings = {
         "url": queryUrl,
         "method": "GET"
     };
+    $("#welcomeImg").hide(3000);
     $.ajax(settings).done(function (response) {
-        console.log(response);
         var container = $(".container");
         $("#welcome").empty();
         container.empty();
         var resultsTitle = $("<h1>");
-        resultsTitle.text("Recipe Helper. Showing Results for: " + foodDesc)
+        resultsTitle.text("Recipe search. Showing results for: " + searchInput);
         resultsTitle.addClass("text-center");
         $("#welcome").append(resultsTitle);
         var refreshLink = $("<a>");
@@ -22,6 +32,13 @@ $("#searchBtn").click(function () {
         refreshLink.addClass("text-center");
         $("#welcome").append(refreshLink);
         var objHits = response.hits;
+        if (objHits.length === 0) {
+            var errMessage = $("<div>");
+            errMessage.addClass("text-danger display-4");
+            errMessage.text("Nothing was found! Please try again!");
+            container.append(errMessage);
+            return;
+        }
         objHits.forEach(element => {
             //Recipe Title (Row)
             var h1 = $("<h3>").text(element.recipe.label);
@@ -36,7 +53,7 @@ $("#searchBtn").click(function () {
             newRow.append(newCol);
             var img = $("<img>").attr("src", element.recipe.image);
             newCol.append(img);
-            var calories = $("<div>").text("Calories: " + element.recipe.calories);
+            var calories = $("<div>").text("Calories: " + Math.floor(element.recipe.calories, 2));
             newCol.append(calories);
             var a = $("<a>").attr("href", element.recipe.url).text("Click here for the complete recipe");
             newCol.append(a);
@@ -51,7 +68,12 @@ $("#searchBtn").click(function () {
                 var li = $("<li>").text(element);
                 newUL.append(li);
             });
-            
+            var cautions = element.recipe.cautions.toString();
+            if (cautions) {
+                cautions = $("<div>").text("Cautions: " + cautions);
+                cautions.addClass("text-center text-danger");
+                newCol.append(cautions);
+            }
             //Column 3 (ingredients)
             var newCol = $("<div>");
             newCol.addClass("col");
@@ -66,38 +88,71 @@ $("#searchBtn").click(function () {
             container.append("<hr>");
         });
     });
-});
-
-
-/*Food Database API*/
-var appId_FDA = "e76a4cac";
-var appKey_FDA = "f2a93999d9e4e21df74a6f5e268443c1";
-var foodData = "";
-var queryUrl = "https://api.edamam.com/api/nutrition-data?app_id=$" + appId_FDA + "&app_key=$" + "&ingr=" + foodData;
-var settings = {
-	"url": queryUrl,
-	"method": "GET"
-};
-
-console.log(settings.url);
-
-$.ajax(settings).done(function (response) {
-    console.log(response);
-    var container = $(".container");
-    $("#welcome").empty();
-    container.empty();
-    var resultsTitle = $("<h1>");
-    resultsTitle.text("Food Database. Showing Results for: " + foodData)
-    resultsTitle.addClass("text-center");
-    $("#welcome").append(resultsTitle);
-    var refreshLink = $("<a>");
-    refreshLink.attr("href", "index.html").text("Click here to do another search");
-    refreshLink.addClass("text-center");
-    $("#welcome").append(refreshLink);
-    var objHits = response.hits;
-    objHits.forEach(element => { //Recipe Title (Row)
-        var h1 = $("<h3>").text(element.recipe.label);
-        h1.addClass("text-center");
-        container.append(h1);
-    })
-});
+}
+/*This function uses the Food Database Search API from edaman and fill container div with detailed nutrients information*/
+function foodSearch() {
+    var appId_FDA = "a8e0eb85";
+    var appKey_FDA = "f696fd92e5f6ee3ba12d0d190084dd1e";
+    var searchInput = $("#searchInput").val();
+    var queryUrl = "https://api.edamam.com/api/food-database/v2/parser?ingr=" + searchInput + "&app_id=" + appId_FDA + "&app_key=" + appKey_FDA;
+    var settings = {
+        "url": queryUrl,
+        "method": "GET"
+    };
+    $("#welcomeImg").slideUp(3000);
+    $.ajax(settings).done(function (response) {
+        var container = $(".container");
+        $("#welcome").empty();
+        container.empty();
+        var resultsTitle = $("<h1>");
+        resultsTitle.text("Food search. Showing results for: " + searchInput)
+        resultsTitle.addClass("text-center");
+        $("#welcome").append(resultsTitle);
+        var refreshLink = $("<a>");
+        refreshLink.attr("href", "index.html").text("Click here to do another search");
+        refreshLink.addClass("text-center");
+        $("#welcome").append(refreshLink);
+        if (response.hints.length === 0) {
+            var errMessage = $("<div>");
+            errMessage.addClass("text-danger display-4");
+            errMessage.text("Nothing was found! Please try again!");
+            container.append(errMessage);
+            return;
+        }
+        var objInfo = response.hints[0];
+        //Column 1 (Food Image and category)
+        var newRow = $("<div>");
+        newRow.addClass("row");
+        container.append(newRow);
+        var newCol = $("<div>");
+        newCol.addClass("col");
+        newRow.append(newCol);
+        var img = $("<img>").attr("src", objInfo.food.image);
+        newCol.append(img);
+        var category = $("<div>").text("Category: " + objInfo.food.category);
+        newCol.append(category);
+        var foodId = $("<div>").text("Food ID: " + objInfo.food.foodId);
+        newCol.append(foodId);
+        var brand = objInfo.food.brand;
+        if (brand) {
+            var brand = $("<div>").text("Brand: " + brand);
+            newCol.append(brand);
+        }
+        //Column 2 (nutrients / calories)
+        var newCol = $("<div>");
+        newCol.addClass("col");
+        newRow.append(newCol);
+        var newUL = $("<ul>").text("Nutrients for " + objInfo.food.label);
+        newCol.append(newUL);
+        var nutrients = objInfo.food.nutrients;
+        var liEnergy = $("<li>").text("Energy: " + nutrients.ENERC_KCAL + "kcal");
+        newUL.append(liEnergy);
+        var liCarbs = $("<li>").text("Carbs: " + nutrients.CHOCDF + "g");
+        newUL.append(liCarbs);
+        var liProtein = $("<li>").text("Protein: " + nutrients.PROCNT + "g");
+        newUL.append(liProtein);
+        var liProtein = $("<li>").text("Fat: " + nutrients.FAT + "g");
+        newUL.append(liProtein);
+        container.append("<hr>");
+    });
+}
